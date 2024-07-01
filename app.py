2b1a5fe5a8ee019e030b5c7c57a263706b3be6e9
Antonio Coca <71002543+ToniCo99@users.noreply.github.com>
2024-07-01 18:06:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import os
 import pandas as pd
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
-
 
 files = [
     'almeria.csv', 'cadiz.csv', 'cordoba.csv', 'granada.csv',
@@ -18,19 +18,13 @@ for file in files:
     df["Provincia"] = province.capitalize()
     dfs.append(df)
 
-# Combinar todos los DataFrames en uno solo
 df = pd.concat(dfs)
-
-# Convertir la columna de volumen a numérico
 df['Volumen'] = df['Volumen'].str.replace('.', '').str.replace(',', '.').astype(float)
 
-
-# Crear la aplicación Dash
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     html.H1("Exportaciones en Andalucía por Provincia y Sector"),
-    
     html.Div([
         html.Label("Seleccione las Provincias"),
         dcc.Dropdown(
@@ -40,7 +34,6 @@ app.layout = html.Div([
             multi=True
         )
     ]),
-    
     html.Div([
         html.Label("Seleccione los Sectores"),
         dcc.Dropdown(
@@ -50,7 +43,6 @@ app.layout = html.Div([
             multi=True
         )
     ]),
-    
     dcc.Graph(id='export-line-chart')
 ])
 
@@ -61,21 +53,15 @@ app.layout = html.Div([
 )
 def update_line_chart(selected_provinces, selected_sectors):
     filtered_df = df[df['Provincia'].isin(selected_provinces) & df['Sector'].isin(selected_sectors)]
-    
-    # Agrupar por Año y sumar los volúmenes
     aggregated_df = filtered_df.groupby(['Año']).sum().reset_index()
-
-    # Crear el gráfico lineal
     fig = px.line(aggregated_df, x='Año', y='Volumen', title='Sumatorio de Exportaciones por Año')
-
-    # Actualizar las etiquetas de los ejes y los valores para mostrar que son en euros
     fig.update_layout(
         yaxis_title='Volumen de Exportaciones (€)',
         xaxis_title='Año'
     )
     fig.update_traces(hovertemplate='Año=%{x}<br>Volumen=%{y:,.2f} €')
-
     return fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=True, host='0.0.0.0', port=port)
